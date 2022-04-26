5617943af42de88b9146d369e8ecf083911468e0
Martien Dermawan Tanama <martiendt@gmail.com>
2022-04-26 13:53:24 +0700

    Periodic Service Worker Updates

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,15 @@ import './index.css';
 
 import { registerSW } from 'virtual:pwa-register';
 
+const intervalMS = 60 * 60 * 1000;
+
 const updateSW = registerSW({
+  onRegistered(r) {
+    r &&
+      setInterval(() => {
+        r.update();
+      }, intervalMS);
+  },
   onNeedRefresh() {},
   onOfflineReady() {},
   onRegisterError(error) {},
