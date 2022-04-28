abad96dff9af81636b5a602258dadccb563d8b71
Martien Dermawan Tanama <martiendt@gmail.com>
2022-04-29 06:31:33 +0700

    b

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,49 @@ import router from './router';
 import App from './App.vue';
 import './index.css';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('sw.js')
+    .then(function (reg) {
+      console.log('Service Worker Registered!', reg);
+
+      reg.pushManager.getSubscription().then(function (sub) {
+        if (sub === null) {
+          // Update UI to ask user to register for Push
+          subscribeUser();
+          console.log('Not subscribed to push service!');
+        } else {
+          // We have a subscription, update the database
+          console.log('Subscription object: ', sub);
+        }
+      });
+    })
+    .catch(function (err) {
+      console.log('Service Worker registration failed: ', err);
+    });
+}
+
+function subscribeUser() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(function (reg) {
+      reg.pushManager
+        .subscribe({
+          userVisibleOnly: true,
+        })
+        .then(function (sub) {
+          console.log('Endpoint URL: ', sub.endpoint);
+        })
+        .catch(function (e) {
+          if (Notification.permission === 'denied') {
+            console.warn('Permission for notifications was denied');
+          } else {
+            console.error('Unable to subscribe to push', e);
+          }
+        });
+    });
+  }
+}
+
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
--- src/modules/base/pages/home.vue
+++ src/modules/base/pages/home.vue
@@ -6,9 +6,25 @@ interface Props {
 const props = withDefaults(defineProps<Props>(), {
   name: 'John Doe',
 });
+
+addEventListener('load', async () => {
+  let sw = await navigator.serviceWorker.register('./sw.js');
+  console.log(sw);
+});
+
+const subscribe = async () => {
+  let sw = await navigator.serviceWorker.ready;
+  let push = await sw.pushManaget.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey:
+      'BG4xOT-4Jk4fbY6RKe7CrsXYXAVi9jpG2WLfW48mXu0ci22gS5ApJYUR00lvFKR0RhArecYDHphGw_0WXptwfaM',
+  });
+  console.log(JSON.stringify(push));
+};
 </script>
 
 <template>
   <div>Home</div>
   <p>Hello {{ name }}</p>
+  <button @click="subscribe()">subscribe</button>
 </template>
