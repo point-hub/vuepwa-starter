3a7a88ea262d3233772b1c8830ae956055288570
Martien Dermawan Tanama <martiendt@gmail.com>
2022-04-29 11:29:52 +0700

    update sw

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,26 @@ if ('serviceWorker' in navigator) {
     .catch(function (err) {
       console.log('Service Worker registration failed: ', err);
     });
+
+  // Register push api service worker
+  navigator.serviceWorker
+    .register('/push-api/pointhub/pointhubPAServiceWorker.js')
+    .then(function (reg) {
+      console.log('Service Worker Registered!', reg);
+
+      reg.pushManager.getSubscription().then(function (sub) {
+        if (sub === null) {
+          // Update UI to ask user to register for Push
+          console.log('Not subscribed to push service!');
+        } else {
+          // We have a subscription, update the database
+          console.log('Subscription object: ', sub);
+        }
+      });
+    })
+    .catch(function (err) {
+      console.log('Service Worker registration failed: ', err);
+    });
 }
 
 function subscribeUser() {
