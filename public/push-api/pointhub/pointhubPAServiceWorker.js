d6423e5f6f8eb31a18a9d1a3e308f831865a3cf1
Martien Dermawan Tanama <martiendt@gmail.com>
2022-04-29 11:18:15 +0700

    use different service worker

---
+++ public/push-api/pointhub/pointhubPAServiceWorker.js
@@ -0,0 +1,41 @@
+import {
+  cleanupOutdatedCaches,
+  createHandlerBoundToURL,
+  precacheAndRoute,
+} from 'workbox-precaching';
+import { NavigationRoute, registerRoute } from 'workbox-routing';
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+});
+
+// self.__WB_MANIFEST is default injection point
+precacheAndRoute(self.__WB_MANIFEST);
+
+// clean old assets
+cleanupOutdatedCaches();
+
+// to allow work offline
+registerRoute(new NavigationRoute(createHandlerBoundToURL('index.html')));
+
+// Web Push Notifications
+self.addEventListener('push', function (e) {
+  const options = {
+    body: 'This notification was generated from a push!',
+    icon: 'images/example.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '2',
+    },
+    actions: [
+      {
+        action: 'explore',
+        title: 'Explore this new world',
+        icon: 'images/checkmark.png',
+      },
+      { action: 'close', title: 'Close', icon: 'images/xmark.png' },
+    ],
+  };
+  e.waitUntil(self.registration.showNotification('Hello world!', options));
+});
--- src/sw.ts
+++ src/sw.ts
@@ -19,25 +19,3 @@ cleanupOutdatedCaches();
 
 // to allow work offline
 registerRoute(new NavigationRoute(createHandlerBoundToURL('index.html')));
-
-// Web Push Notifications
-self.addEventListener('push', function (e) {
-  const options = {
-    body: 'This notification was generated from a push!',
-    icon: 'images/example.png',
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: '2',
-    },
-    actions: [
-      {
-        action: 'explore',
-        title: 'Explore this new world',
-        icon: 'images/checkmark.png',
-      },
-      { action: 'close', title: 'Close', icon: 'images/xmark.png' },
-    ],
-  };
-  e.waitUntil(self.registration.showNotification('Hello world!', options));
-});
